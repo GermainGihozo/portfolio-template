a7ff01c1cc95028def31325e3813762c0c780183
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-08-30 14:40:14 +0300

    add contact page

--- src/components/projects.ts
+++ src/components/projects.ts
@@ -1,86 +1,87 @@
 export const projects = [
   {
     id: 1,
-    logo: '/vite.svg',
-    title: 'E-comerce website 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.',
+    logo: "/vite.svg",
+    title: "E-comerce Rest API",
+    description: [
+      "This is Restfull API that can help user to purchase products, they can choose the product an pay them with their prefered payment method.  The admin of the app can add new products, remove them and update them.",
+      "This app has all major functionalities an e-commerce app needs to serve it's purpose.",
+    ],
     images: [
-      'https://cdn.dribbble.com/userupload/6970046/file/original-c5697e287d243d0fcd9bb89db41ba5e8.png?compress=1&resize=1024x735',
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
+      "https://cdn.dribbble.com/userupload/6970046/file/original-c5697e287d243d0fcd9bb89db41ba5e8.png?compress=1&resize=1024x735",
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
     ],
     githubLink:
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
     liveWebsite:
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
-    techStacks: ['nodejs', 'expressjs', 'reactjs', 'tailwindcss', 'postgresql'],
-    linksToolTipData: {
-      description: [
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 1',
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 3',
-      ],
-      skillsGained: [
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 1',
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 3',
-      ],
-    },
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
+    techStacks: [
+      "Typescript",
+      "expressjs",
+      "Postgres DB",
+      "Sequelize ORM",
+      "docker",
+      "jest",
+      "stripe",
+    ],
     skillsGained:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.Lorem ipsum dolor <a href="/test" class="link">sit amet</a>, consectetur adipisicing elit, sed do eiusmod tempor incid.',
+      "Building this project has gave me many skills like teamwork skills, collaboration, devops, database design, testing, writting typesafe code, backend using nodejs and express, websockets and increased my experience with git and github.",
   },
   {
     id: 2,
-    logo: '/vite.svg',
-    title: 'E-comerce website 2',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.',
+    logo: "/vite.svg",
+    title: "E-comerce Front-end",
+    description: [
+      "This is the front-end of above ecommerce API, it has the main features of ecommerce web application like product listing, adding and removing products to cart and payment. adding new product in store and removing them and much more.",
+    ],
     images: [
-      'https://cdn.dribbble.com/userupload/6970046/file/original-c5697e287d243d0fcd9bb89db41ba5e8.png?compress=1&resize=1024x735',
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
+      "https://cdn.dribbble.com/userupload/6970046/file/original-c5697e287d243d0fcd9bb89db41ba5e8.png?compress=1&resize=1024x735",
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
     ],
     githubLink:
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
     liveWebsite:
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
-    techStacks: ['nodejs', 'expressjs', 'reactjs', 'tailwindcss', 'postgresql'],
-    linksToolTipData: {
-      description: [
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 1',
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 3',
-      ],
-      skillsGained: [
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 1',
-        'consectetur adipisicing elit, sed do eiusmod tempor incid 3',
-      ],
-    },
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
+    techStacks: [
+      "React",
+      "TypeScript",
+      "redux",
+      "redux toolkit",
+      "tailwindcss",
+      "socket.io",
+      "jest",
+      "webpack",
+      "storybook",
+    ],
     skillsGained:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.Lorem ipsum dolor <a href="/test" class="link">sit amet</a>, consectetur adipisicing elit, sed do eiusmod tempor incid.',
+      "By building this project it helped me to gain UI/UX and front-end skills: wireframes and prototyping with figma, developing robust web applications with react and redux and ducumenting with storybook.",
   },
   {
     id: 3,
-    logo: '/vite.svg',
-    title: 'E-comerce website 3',
-    description:
-      'Lorem ipsum dolor sit amet, <a href="/test" class="link">consectetur adipisicing</a> elit, sed do eiusmod tempor incid. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.',
+    logo: "/vite.svg",
+    title: "Personal blog Rest API",
+    description: [
+      "User can create and read blog posts, comment on blog posts and like them, they can also like comments. except reading a blog post all other action require user to be registered and loged in.",
+    ],
     images: [
-      'https://cdn.dribbble.com/userupload/6970046/file/original-c5697e287d243d0fcd9bb89db41ba5e8.png?compress=1&resize=1024x735',
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
+      "https://cdn.dribbble.com/userupload/6970046/file/original-c5697e287d243d0fcd9bb89db41ba5e8.png?compress=1&resize=1024x735",
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
     ],
     githubLink:
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
     liveWebsite:
-      'https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768',
-    techStacks: ['nodejs', 'expressjs', 'reactjs', 'tailwindcss', 'postgresql'],
-    linksToolTipData: {
-      description: [
-        'consectetur adipisicing elit, sed do eiusmod tempor incid description 1',
-        'consectetur adipisicing elit, sed do eiusmod tempor incid description 3',
-      ],
-      skillsGained: [
-        'consectetur adipisicing elit, sed do eiusmod tempor incid skillsGained 1',
-        'consectetur adipisicing elit, sed do eiusmod tempor incid skillsGained 3',
-      ],
-    },
+      "https://cdn.dribbble.com/userupload/6968359/file/original-ef3dc98af34d859ac1e8edb594ace1c0.png?compress=1&resize=1024x768",
+    techStacks: [
+      "express",
+      "mongodb",
+      "mongoose",
+      "redis",
+      "mocha",
+      "passport",
+      "chai",
+      "joi",
+    ],
     skillsGained:
-      'Lorem ipsum dolor sit amet, <a href="/test" class="link">consectetur adipisicing</a> elit, sed do eiusmod tempor incid. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid.Lorem ipsum dolor <a href="/test" class="link">sit amet</a>, consectetur adipisicing elit, sed do eiusmod tempor incid.',
+      "I learned git and github, test-Driven development, project management tool(pivotal tracker and Trello), clean coding and modern javascript, best Practices for a pragmatic RESTful API, Continuous Integration.",
   },
 ];
