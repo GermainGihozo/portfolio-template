e94dc7a4b0f97f94698f4e14c3bbca3ac7456997
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-09-11 17:50:17 +0300

    add contact feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-
-import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  output: "hybrid",
+  adapter: netlify(),
 });
--- src/pages/contact.json.ts
+++ src/pages/contact.json.ts
@@ -1,7 +1,31 @@
 import type { APIRoute } from "astro";
+import nodemailer from "nodemailer";
 
-export const GET: APIRoute = () => {
-  return new Response(
-    JSON.stringify({ message: "Muslim will reply in 3 hours." })
-  );
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST!,
+  port: Number(process.env.EMAIL_PORT!),
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER!,
+    pass: process.env.EMAIL_PASSWORD!,
+  },
+});
+export const POST: APIRoute = async ({ request, redirect }) => {
+  try {
+    const data = await request.formData();
+    const email = data.get("email")?.toString();
+    const message = data.get("message")?.toString();
+    if (!email || !message) return new Response(null, { status: 400 });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      text: message,
+      subject: message.slice(0, 25),
+    });
+
+    return redirect("/", 307);
+  } catch (error) {
+    return new Response(null, { status: 400 });
+  }
 };
